267fd65a0c64e54d89b6e1123b41dc7de838a76a
krrish <87867238+krrish-v@users.noreply.github.com>
2022-06-24 20:25:49 +0530

    Update engine.py

--- LIBSER/libser_engine/engine.py
+++ LIBSER/libser_engine/engine.py
@@ -2,14 +2,9 @@
 import urllib3
 import requests
 from bs4 import BeautifulSoup
-from nltk.tokenize import word_tokenize, sent_tokenize, RegexpTokenizer
-from string import punctuation
 from utils import payload
 from utils.exception import GoogleCaptcha, GoogleCookiePolicies
-from utils.agent import user_agent
 import re
-
-filetype = ('pdf','doc','xls','ppt','odp','ods','docx','xlsx','pptx')
 
 
 class Extract():
@@ -29,7 +24,7 @@ class Extract():
 			self.data = req.text
 			status = req.status_code
 
-            # data extrcation from search page
+            		# data extrcation from search page
 			if status == 200:
 				soup = BeautifulSoup(self.data, 'html.parser')
 
@@ -61,7 +56,7 @@ class Extract():
 		except: return False
 
 	#______________________________________
-	# Google API
+	# Google
 	#-------------------------------------
 	def google_search(self, target, total, filetype):
 		self.word = target
@@ -72,62 +67,36 @@ class Extract():
 		urllib3.disable_warnings()
 
 		documents = []
-		start = 0
 		num = 50 if total > 50 else total
-		iterations = int(total/50)
-		if (total%50) != 0:
-			iterations += 1
 
 		url_base = f"https://www.google.com"
 		#cookies = {"CONSENT": "YES+srp.gws"}
 
 		header = payload.Headers().google_parm()
 		
-		while (start < iterations) and (len(documents) < total):
-			try:
-				'''
-				url = url_base + f"/search?num="+self.quantity+"&start=" + str(self.counter) + "&hl=en&meta=&q=filetype:"+self.filetype+"%20allintext:" + self.word
+		try:
 				
-				response = requests.get(url, headers=header, timeout=5, verify=False, allow_redirects=False)
-				text = response.text
-				content = respomse.content
-				print(text)
-				
-				if response.status_code == 302 and ("htps://www.google.com/webhp" in text or "https://consent.google.com" in text):
-					raise GoogleCookiePolicies()
-				if "detected unusual traffic" in text:
-					raise GoogleCaptcha()'''
-				
-				f = open('duck.txt', 'r')
-				content = f.read()
-				f.close()
-				
-				soup = BeautifulSoup(content, features="lxml")
-				links = soup.findAll("a")
-				for link in  soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
-					link = re.split(":(?=http)",link["href"].replace("/url?q=",""))
-					documents.append(link)
+			url = url_base + f"/search?num="+self.quantity+"&start=" + str(self.counter) + "&hl=en&meta=&q=filetype:"+self.filetype+"%20allintext:" + self.word
+			
+			response = requests.get(url, headers=header, timeout=5, verify=False, allow_redirects=False)
+			
+			text = response.text
+			content = response.content
+			
+			if response.status_code == 302 and ("htps://www.google.com/webhp" in text or "https://consent.google.com" in text):
+				raise GoogleCookiePolicies()
+			if "detected unusual traffic" in text:
+				raise GoogleCaptcha()
+			
+			soup = BeautifulSoup(content, features="lxml")
+			
+			for link in  soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
+				link = re.split(":(?=http)",link["href"].replace("/url?q=",""))
 
-					if len(documents) >= total:
-						break
+				filtered_link = link[0].split('&sa=')
+				documents.append(filtered_link[0])
 
-			except Exception as ex:
-				raise ex #It's left over... but it stays there
-			start += 1
+		except Exception as ex:
+			raise ex
+			
 		return documents
-
-
-#print(Extract().duckduckgo_search('mental maths'))
-a = Extract().google_search('india', 30, 'pdf')
-for q in a:
-	print(q)
-
-
-#page = requests.get("https://www.google.dz/search?q=see")
-#soup = BeautifulSoup(page.content)
-#import re
-#links = soup.findAll("a")
-#for link in  soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
-#	print(re.split(":(?=http)",link["href"].replace("/url?q=","")))
-
-
